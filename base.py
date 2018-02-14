34669930f3557bc9f5efb820b5cfe078aff14e2e
Jordan Baker <jordanmitchellbaker@gmail.com>
2018-02-13 20:31:44 -0600

    ...

--- base.py
+++ base.py
@@ -118,6 +118,8 @@ submission.to_csv('submission1.csv', index=False)
 #==============================================================================
 # TO DO
 
+
+
 # better commenting/explanations
 # check out model zoo? or something similar to that?
 # jupyter?
@@ -132,11 +134,19 @@ submission.to_csv('submission1.csv', index=False)
 #==============================================================================
 
 
+
+
+
+
+
+
 #==============================================================================
 # REFERENCES
 # https://www.kaggle.com/sbongo/for-beginners-tackling-toxic-using-keras/notebook
 # new base model? https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/discussion/47964
 #==============================================================================
+
+
 
 
 
